49ec48fd5f31b476467ae8fb4fb4f55301d817ec
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2017-09-21 10:37:15 -0300

    Adds documentation in parser

--- parsers/pokemon.js
+++ parsers/pokemon.js
@@ -1,3 +1,10 @@
+/**
+ * Creates a pokemon object using the model
+ * @param {Pokemon} pokemon The pokemon to parse
+ * @param {string} pokemon.name The name of the pokemon
+ * @param {price} pokemon.price The price of the pokemon
+ * @param {stock} pokemon.stock The stock of the pokemon
+ */
 const parse = ({ name, price, stock }) => {
   const pokemonParsed = {};
 
