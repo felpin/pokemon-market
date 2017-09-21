e1c3d351876a3fd2798b4020ffe0a5912f5084de
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2017-09-21 09:53:05 -0300

    Changes the server to use HTTPS instead of HTTP

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
 const router = require('./config/router');
 const sequelize = require('./config/sequelize');
-
 
 const app = express();
 
@@ -12,4 +13,10 @@ app.use(bodyParser.json());
 app.use('/', router);
 
 sequelize.sync({ force: true });
-app.listen(3000);
+
+const sslCredentials = {
+  key: fs.readFileSync(process.env.SSL_KEY),
+  cert: fs.readFileSync(process.env.SSL_CRT),
+};
+
+https.createServer(sslCredentials, app).listen(process.env.APP_PORT);
