c8c76fb0983172c8a0c79d6e546b30d7a3c04d3a
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2017-09-21 10:37:43 -0300

    Adds documentation in utils

--- utils/errorhandler.js
+++ utils/errorhandler.js
@@ -3,19 +3,29 @@ const PagarmeTransactionError = require('../errors/PagarmeTransactionError');
 const UnexistentPokemonError = require('../errors/UnexistentPokemonError');
 const UnpaidTransactionError = require('../errors/UnpaidTransactionError');
 
-const handle = res => (error) => {
-  switch (error.name) {
-    case PagarmeTransactionError.name:
-    case UnpaidTransactionError.name:
-      res.status(400).send(error);
-      break;
-    case NotEnoughStockOfPokemonError.name:
-    case UnexistentPokemonError.name:
-      res.status(422).send(error);
-      break;
-    default:
-      res.sendStatus(500);
-  }
-};
+/**
+ * Creates a function to handle errors
+ * @param {Object} res The express' response object
+ * @returns {function} A function to handle errors
+ */
+const handle = res =>
+  /**
+   * Handle errors
+   * @param {Error} error The error
+   */
+  (error) => {
+    switch (error.name) {
+      case PagarmeTransactionError.name:
+      case UnpaidTransactionError.name:
+        res.status(400).send(error);
+        break;
+      case NotEnoughStockOfPokemonError.name:
+      case UnexistentPokemonError.name:
+        res.status(422).send(error);
+        break;
+      default:
+        res.sendStatus(500);
+    }
+  };
 
 module.exports = handle;
